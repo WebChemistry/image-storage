366797337583fcefe95b29e6bdb28d85d3d9e387
MartkCz <martk494@gmail.com>
2020-05-11 16:09:07 +0200

    ImageType use DatabaseConverter

--- src/Doctrine/ImageType.php
+++ src/Doctrine/ImageType.php
@@ -5,28 +5,38 @@ namespace WebChemistry\ImageStorage\Doctrine;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\StringType;
+use WebChemistry\ImageStorage\Database\DatabaseConverter;
+use WebChemistry\ImageStorage\Database\DatabaseConverterInterface;
 use WebChemistry\ImageStorage\Entity\ImageInterface;
-use WebChemistry\ImageStorage\Entity\PersistentImage;
 
 class ImageType extends StringType
 {
+
+	private DatabaseConverterInterface $databaseConverter;
+
+	public function getDatabaseConverter(): DatabaseConverterInterface
+	{
+		if (!isset($this->databaseConverter)) {
+			$this->databaseConverter = new DatabaseConverter();
+		}
+
+		return $this->databaseConverter;
+	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function convertToDatabaseValue($value, AbstractPlatform $platform)
 	{
-		if ($value instanceof ImageInterface) {
-			return $value->getId();
-		} elseif ($value === null) {
-			return $value;
+		if (!$value instanceof ImageInterface || $value !== null) {
+			throw ConversionException::conversionFailedInvalidType(
+				$value,
+				$this->getName(),
+				['null', ImageInterface::class]
+			);
 		}
 
-		throw ConversionException::conversionFailedInvalidType(
-			$value,
-			$this->getName(),
-			['null', ImageInterface::class]
-		);
+		return $this->getDatabaseConverter()->convertToDatabase($value);
 	}
 
 	/**
@@ -34,11 +44,15 @@ class ImageType extends StringType
 	 */
 	public function convertToPHPValue($value, AbstractPlatform $platform) // phpcs:ignore Generic.NamingConventions.CamelCapsFunctionName.ScopeNotCamelCaps
 	{
-		if ($value === null) {
-			return $value;
+		if (!is_string($value) || $value !== null) {
+			throw ConversionException::conversionFailedInvalidType(
+				$value,
+				$this->getName(),
+				['null', 'string']
+			);
 		}
 
-		return new PersistentImage((string) $value);
+		return $this->databaseConverter->convertToPhp($value);
 	}
 
 	/**
