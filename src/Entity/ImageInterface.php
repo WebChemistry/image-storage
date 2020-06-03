b0ed60e7df5399bf9f9e863cfc4b04872b0a84ee
MartkCz <martk494@gmail.com>
2020-06-03 12:15:20 +0200

    add doc

--- src/Entity/ImageInterface.php
+++ src/Entity/ImageInterface.php
@@ -8,23 +8,52 @@ use WebChemistry\ImageStorage\Scope\Scope;
 interface ImageInterface
 {
 
+	/**
+	 * Combination of scope and name
+	 * @example scope/image.jpg
+	 */
 	public function getId(): string;
 
+	/**
+	 * Name of image
+	 * @example image.jpg
+	 */
 	public function getName(): string;
 
+	/**
+	 * Suffix of image
+	 * @example jpg
+	 */
 	public function getSuffix(): ?string;
 
+	/**
+	 * Scope of image
+	 */
 	public function getScope(): Scope;
 
+	/**
+	 * Filter object of image
+	 */
 	public function getFilter(): ?FilterInterface;
 
+	/**
+	 * Checks if image has filter
+	 */
 	public function hasFilter(): bool;
 
+	/**
+	 * Checks if image is closed
+	 */
 	public function isClosed(): bool;
 
+	/**
+	 * Checks if image is empty
+	 */
 	public function isEmpty(): bool;
 
 	/**
+	 * Returns image without filter
+	 *
 	 * @return static
 	 */
 	public function getOriginal();
